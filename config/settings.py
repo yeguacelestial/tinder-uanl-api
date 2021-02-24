ad169c5ff3fbbdb25b4252420c62b01c05fb8b73
Yegua Celestial <yeguacelestial@gmail.com>
2021-02-24 15:02:46 -0600

    Add staging host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'tinder-uanl-staging.herokuapp.com',
+]
 
 
 # Application definition
